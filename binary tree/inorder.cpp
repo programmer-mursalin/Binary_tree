fbe29f291320d1df06c75e6f2e10b878b5f4ca78
Mursalin Hawlader <programmer.mursalin016@gmail.com>
2023-09-26 01:44:40 +0600

    inorder.cpp

---
+++ binary tree/inorder.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int val;
+    node *left;
+    node *right;
+    node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void inorder(node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+int main()
+{
+    node *root = new node(10);
+    node *a = new node(20);
+    node *b = new node(30);
+    node *c = new node(40);
+    node *d = new node(50);
+    node *e = new node(60);
+    node *f = new node(70);
+    node *g = new node(80);
+    node *h = new node(90);
+    node *i = new node(100);
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    h->right = i;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+    inorder(root);
+}
